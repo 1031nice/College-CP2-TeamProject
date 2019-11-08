33fc93902a26418fd4f9ce476ad9b9b06f02a635
KimAYoung <onionlily123@naver.com>
2019-11-08 20:44:31 +0900

    change AppController to interface

--- ParkingLotApplication/src/parkingLotApplication/AppController.java
+++ ParkingLotApplication/src/parkingLotApplication/AppController.java
@@ -1,19 +1,6 @@
 package parkingLotApplication;
 
-public class AppController {
-	private String id;
-	private String password;
-	
-	public void register(String id, String password) {
-		this.id = id;
-		this.password = password;
-	}
-	
-	public void changeInfo(String id, String password) {
-		this.id = id;
-		this.password = password;
-	}
-	
-	
-	
+public interface AppController {
+	public void resiter();
+	public void changeInfo();
 }
