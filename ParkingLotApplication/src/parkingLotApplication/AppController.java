12bbe62e97bff4983e126a11a27ea6222f4b7c2b
KimAYoung <onionlily123@naver.com>
2019-11-08 20:27:04 +0900

    change AppController

--- ParkingLotApplication/src/parkingLotApplication/AppController.java
+++ ParkingLotApplication/src/parkingLotApplication/AppController.java
@@ -1,5 +1,18 @@
 package parkingLotApplication;
 
 public class AppController {
-
+	private String id;
+	private String password;
+	
+	public void register(String id, String password) {
+		this.id = id;
+		this.password = password;
+	}
+	
+	public void changeInformation(String id, String password) {
+		this.id = id;
+		this.password = password;
+	}
+	
+	
 }
