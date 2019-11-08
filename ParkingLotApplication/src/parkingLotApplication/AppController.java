e6d040c2106c796c19f1da25e3b5eba9e2696ad6
KimAYoung <onionlily123@naver.com>
2019-11-08 20:34:33 +0900

    change A

--- ParkingLotApplication/src/parkingLotApplication/AppController.java
+++ ParkingLotApplication/src/parkingLotApplication/AppController.java
@@ -3,6 +3,7 @@ package parkingLotApplication;
 public class AppController {
 	private String id;
 	private String password;
+	private String a;
 	
 	public void register(String id, String password) {
 		this.id = id;
