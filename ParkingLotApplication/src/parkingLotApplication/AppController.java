d9aa77b5d7d9f53f51ab7ddc21d1e43844157c77
KimAYoung <onionlily123@naver.com>
2019-11-14 19:51:47 +0900

    change AppController

--- ParkingLotApplication/src/parkingLotApplication/AppController.java
+++ ParkingLotApplication/src/parkingLotApplication/AppController.java
@@ -1,7 +1,8 @@
 package parkingLotApplication;
 
 public interface AppController {
-	public void register();
-	public void changeInfo();
+	public void SignUp();  //회원가입
+	public void EditInfo(); //회원정보변경
+	public void MemberSecession(); //회원탈퇴
 	
 }
