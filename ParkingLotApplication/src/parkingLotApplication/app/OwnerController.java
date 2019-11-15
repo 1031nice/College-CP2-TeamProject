8531b7813f155d7f55f4c021da1fe89fa9356b9e
KimAYoung <onionlily123@naver.com>
2019-11-15 23:54:20 +0900

    change AppController(add IOException) and change Owner register method

--- ParkingLotApplication/src/parkingLotApplication/app/OwnerController.java
+++ ParkingLotApplication/src/parkingLotApplication/app/OwnerController.java
@@ -10,7 +10,7 @@ public class OwnerController extends AppController {
 	
 	@Override
 	public void signUp() throws IOException{
-		Owner owner1 = new Owner("id5", "pw", "name", 21, 25222937); //임시 객체 생성	
+		Owner owner1 = new Owner("id5", "pw", "name", 21, 25222937); //임시로 객체 생성 	
 		ownerIn = new FileWriter("OwnerInfo.txt");
 		ownerIn.write("Id:"+owner1.getId() + " Pw:" + owner1.getPassword() + " Name:" + owner1.getName() + " Age:" + owner1.getAge() + " PhoneNumber:" + owner1.getPhoneNumber());
 		ownerIn.write("\n"); //개행이 안됨 나중에 코드 고치기!
