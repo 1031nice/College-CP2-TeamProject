f9eb79b2cc881f2a21fb3f467fd1ff0bac1713a6
kimseongyu <kskksk0925@naver.com>
2019-11-30 23:45:59 +0900

    parkinglotcontroller

--- ParkingLotApplication/src/parkingLotApplication/GUI/UserMainController.java
+++ ParkingLotApplication/src/parkingLotApplication/GUI/UserMainController.java
@@ -1,17 +1,26 @@
 package parkingLotApplication.GUI;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Optional;
+import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.layout.AnchorPane;
+import parkingLotApplication.model.ParkingLot;
+import javafx.scene.control.Label;
 
-public class UserMainController {
+public class UserMainController implements Initializable{
 
 	@FXML Button reservationButton;
 	@FXML Button returnButton;
@@ -20,6 +29,13 @@ public class UserMainController {
 	@FXML Button logoutButton;
 	@FXML Button exitButton;
 	@FXML AnchorPane anchorPane;
+	@FXML Label userName;
+	@FXML Label parkingLotName;
+	private ArrayList<ParkingLot> list;
+	FileInputStream fis = null;
+	BufferedInputStream bis = null;
+	ObjectInputStream ois = null;
+	
 	@FXML public void reservationAction() {
 		
 	}
@@ -41,5 +57,26 @@ public class UserMainController {
 	@FXML public void exitAction() {
 		System.exit(1);
 	}
-
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		try {
+			fis  = new FileInputStream("./src/parkingLotApplication/app/ClientInfo.txt");
+			bis = new BufferedInputStream(fis);
+			ois = new ObjectInputStream(bis);
+			list = (ArrayList<ParkingLot>) ois.readObject();
+			
+			fis.close();
+			bis.close();
+			ois.close();
+		}  catch (IOException e) {} catch (ClassNotFoundException e) {}
+		
+		for(int i = 0; i < list.size(); i++) {
+			if(user.getName().equls(list.get(i).getName())) {
+				userName.setText(user.getName());
+			}
+			if() {
+				
+			}
+		}
+	}
 }
