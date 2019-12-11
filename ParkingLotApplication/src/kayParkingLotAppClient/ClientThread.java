bc3264802111b83a04286ec708dc7d946f5a1343
KimAYoung <57066971+KimAYoung-Lily@users.noreply.github.com>
2019-12-12 00:40:12 +0900

    Client 필요없는 주석 삭제

--- ParkingLotApplication/src/kayParkingLotAppClient/ClientThread.java
+++ ParkingLotApplication/src/kayParkingLotAppClient/ClientThread.java
@@ -20,12 +20,12 @@ public class ClientThread extends Thread {
 				Socket socket = new Socket(host, port);
 				
 				ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-				oos.writeObject(user); //입출력을 확인하기 위해 User객체대신 임시적으로 String을 사용했습니다.
+				oos.writeObject(user); 
 				oos.flush();
 				
 				ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
 				User user = (User) ois.readObject(); 
-				System.out.println(user.getName() + " 을 가진 객체를 서버로 부터 받음");
+				System.out.println(user.getName() + " 의 이름을 가진 객체를 서버로 부터 받음");
 				
 			} catch (Exception e) {e.printStackTrace();}	
 		}
